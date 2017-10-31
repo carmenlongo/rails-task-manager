3e3196c7a15e25a7c4c9351981374d4600219dd4
Carmen Longo <carmenlongo@hotmail.com>
2017-10-31 14:55:24 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  # # read all tasks => get 'tasks/index'
+  # get "tasks", to: "tasks#index"
+
+  # # read one task => get 'tasks/show'
+  # get "tasks/:id", to: "tasks#show"
+
+  # # create a task => get 'tasks/new' & get 'tasks/create'
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+
+  # # update a task => get 'tasks/edit' & get 'tasks/update'
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # delete a task => get 'tasks/destroy'
+  # delete "tasks/:id", to: "tasks#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create({ title: 'Star Wars', content: 'Lord of the Rings' })
+Task.create({ title: 'Star', content: 'Rings' })
+Task.create({ title: 'Wars', content: 'Lord' })
+
